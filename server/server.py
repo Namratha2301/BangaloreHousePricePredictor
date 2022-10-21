87959dce136f13a5b2c7a8c6a1fcdc94cb93cc1d
Namratha2301 <namratha2301@gmail.com>
2022-10-21 22:35:43 +0530

    Basic App Init

---
+++ server/server.py
@@ -0,0 +1,13 @@
+import utils
+from flask import Flask, request, jsonify, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('app.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
