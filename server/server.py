d021820446cfad92312f44bbb83d6801d9835af4
Namratha2301 <namratha2301@gmail.com>
2022-10-21 22:43:05 +0530

    Added API Endpoints

--- server/server.py
+++ server/server.py
@@ -1,7 +1,18 @@
+import utils
 from flask import Flask, request, jsonify, render_template
 
 
 app = Flask(__name__)
+
+
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': utils.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 
 @app.route('/')
@@ -9,5 +20,20 @@ def home():
     return render_template('app.html')
 
 
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': utils.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
 if __name__ == "__main__":
+    utils.load_saved_artifacts()
     app.run(host='0.0.0.0')
